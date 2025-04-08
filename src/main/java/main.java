673c96a83f0febf6b4de537a0a6aac9cae923266
SIMONHURNEY-STUDENT <g00286398@atu.ie>
2025-04-08 10:20:03 +0100

    Basic version working no tested fail cases;

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,29 +12,51 @@ public class main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter Title");
-        Title = sc.nextLine();
-        if(Title == "Mr"){
-            myPassenger.setTitle(Title);
-        }
-        if(Title == "Mrs"){
-            myPassenger.setTitle(Title);
-        }
-        if(Title == "Ms"){
-            myPassenger.setTitle(Title);
-        }
-        else
-            throw new IllegalArgumentException("Title must be Mr, Mrs or Ms");
+        Title = sc.next();
+        myPassenger.setTitle(Title);
 
+        sc.nextLine();
         System.out.println("Please enter First name ");
-        Fname = sc.nextLine();
+        Fname = sc.next();
         if(Fname.length()<3){
             throw new IllegalArgumentException("Name must be at least 3 characters");
         }
         else
             myPassenger.setFname(Fname);
 
+        sc.nextLine();
+        System.out.println("Please enter last name");
+        Lname = sc.next();
 
-        System.out.println();
+        if(Lname.length()<3){
+            throw new IllegalArgumentException("Name must be at least 3 characters");
+        }
+        else{
+            myPassenger.setLname(Lname);
+        }
+        sc.nextLine();
+        System.out.println("Please enter the age");
+        Age = sc.nextInt();
+
+        if(Age <18){
+            throw new IllegalArgumentException("Must be 18 and older");
+        }
+        else{
+            myPassenger.setAge(Age);
+        }
+        sc.nextLine();
+        System.out.println("Enter Id code");
+        ID = sc.next();
+
+        if(ID.length()<6){
+
+            throw new IllegalArgumentException("ID code must be at least 6 characters");
+
+        }
+        else{
+            myPassenger.setID(ID);
+        }
+
 
 
 
