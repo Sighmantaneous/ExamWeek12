8b0ab637965106e9b6eb1b43a0c3ae066dbd2440
SIMONHURNEY-STUDENT <g00286398@atu.ie>
2025-04-08 09:37:37 +0100

    Adding Passenger class

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,64 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Passenger{
+
+   private String Title, Fname,Lname, ID ;
+   private int Age ;
+
+    public Passenger() {
+        Title = "Mr";
+        Fname = "Test";
+        Lname = "Tester";
+        ID = "100000";
+        Age = 50;
+    }
+
+    public Passenger(String title, String fname, String lname, String ID, int age) {
+        Title = title;
+        Fname = fname;
+        Lname = lname;
+        this.ID = ID;
+        Age = age;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+}
